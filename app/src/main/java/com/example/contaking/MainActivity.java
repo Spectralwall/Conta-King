21257b22513dc0f2e509762a747db1a5b6f592f6
Gabriele <gabrinaretto@libero.it>
2021-01-13 21:01:02 +0100

    Start di progetto con Android Studio, aggiunta nel navigation controller per fragment. App solo fragment

---
+++ app/src/main/java/com/example/contaking/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.contaking;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);//dico che il navigation controller si collegga al navigation host fragment
+    }
+}
---
+++ app/src/main/java/com/example/contaking/NumPlayer.java
@@ -0,0 +1,29 @@
+package com.example.contaking;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class NumPlayer extends Fragment {
+
+    public NumPlayer() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        return inflater.inflate(R.layout.fragment_num_player, container, false);
+    }
+}
